a3805f3abee4ab72c70d4528128f9b5138eac285
Jordan Rocha <70rd23@gmail.com>
2018-08-26 00:09:16 -0300

    Criação do CRUD e relacionamentos do banco de dados

--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -15,6 +15,8 @@
 
 const Route = use('Route')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
